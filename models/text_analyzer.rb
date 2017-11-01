cba27181cf363c1b49e86155396fb40ded7f9a50
Emily Morgan <emorgan05@gmail.com>
2017-11-01 20:42:30 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -23,20 +23,20 @@ class TextAnalyzer
     arr = s1.split('')
     arr1 = arr.uniq
     arr2 = {}
- 
+
     arr1.map do |c|
       arr2[c] =  arr.count(c)
     end
- 
+
     biggest = { arr2.keys.first => arr2.values.first }
- 
+
     arr2.each do |key, value|
       if value > biggest.values.first
         biggest = {}
         biggest[key] = value
       end
     end
- 
+
     biggest
   end
 end
